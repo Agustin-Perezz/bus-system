6e0583d40311e4a81bbe8e6e38d7bf1862d40074
Agustin-Perezz <agustinnicolas806@gmail.com>
2026-08-05 16:44:09 -0300

    Add MikroORM entities and factories for User, Bus, and Trip

---
+++ src/infrastructure/database/postgres/factories/user.factory.ts
@@ -0,0 +1,18 @@
+import { faker } from '@faker-js/faker';
+import { Factory } from '@mikro-orm/seeder';
+import { v7 as uuidv7 } from 'uuid';
+
+import { UserEntity } from '../entities/user.entity';
+
+export class UserFactory extends Factory<UserEntity> {
+  model = UserEntity;
+
+  definition(): Partial<UserEntity> {
+    return {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      role: faker.helpers.arrayElement(['admin', 'driver', 'user']),
+      enterprise: uuidv7(),
+    };
+  }
+}
