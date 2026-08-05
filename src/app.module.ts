a816477cfc33ad919e112527bf73988f2a1d2961
Agustin-Perezz <agustinnicolas806@gmail.com>
2026-08-05 16:51:13 -0300

    Remove book and author registrations from app module and ORM config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { APP_FILTER } from '@nestjs/core';
 import { SentryGlobalFilter } from '@sentry/nestjs/setup';
 
-import { AuthorsModule } from './authors.module';
-import { BooksModule } from './books.module';
 import { BusesModule } from './buses.module';
 import { EnterprisesModule } from './enterprises.module';
 import { DatabaseModule } from './infrastructure/database/database.module';
@@ -12,16 +10,7 @@ import { TripsModule } from './trips.module';
 import { UsersModule } from './users.module';
 
 @Module({
-  imports: [
-    DatabaseModule,
-    BooksModule,
-    AuthorsModule,
-    EnterprisesModule,
-    RoutesModule,
-    UsersModule,
-    BusesModule,
-    TripsModule,
-  ],
+  imports: [DatabaseModule, EnterprisesModule, RoutesModule, UsersModule, BusesModule, TripsModule],
   providers: [
     {
       provide: APP_FILTER,
