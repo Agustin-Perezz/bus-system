0358c6917c5f5cd6af06c124a14e344313a1aaec
Agustin-Perezz <agustinnicolas806@gmail.com>
2026-08-05 16:51:09 -0300

    Remove book and author domain files and books migration

--- src/application/use-cases/books/get-book/get-book.repository.interface.ts
+++
@@ -1,5 +0,0 @@
-import { Book } from '../../../../domain/entities/book.entity';
-
-export interface IGetBookRepository {
-  findById(id: string): Promise<Book | null>;
-}
--- src/application/use-cases/books/list-books/list-books.repository.interface.ts
+++
@@ -1,6 +0,0 @@
-import { Book } from '../../../../domain/entities/book.entity';
-import { PaginationRequestDto } from '../../../shared/dtos/pagination.request.dto';
-
-export interface IListBooksRepository {
-  findAll(pagination: PaginationRequestDto): Promise<[Book[], number]>;
-}
